7b999a6be3c07703deaef7598dcc34b0bcc950e4
Amy Mendee <amymendee@amendee.student.iastate.edu>
2022-01-28 11:58:49 -0600

    Latest update by Amy

--- 553 Group project script.R
+++ 553 Group project script.R
@@ -6,7 +6,6 @@ library(ggplot2)
 
 #Import the dataset
 
-
 #Prepare the dataset for analysis ####
 
 df <- subset(prgusap1_puf, select = c(RACETHN_5CAT, REGION_US, EDCAT8, J_Q09USX))
@@ -25,33 +24,46 @@ summary(das)
 table(das)
 describe(das) 
 
-#Find the mode
-MODE = function(x){ 
-  ta = table(x)
-  tam = max(ta)
-  if (all(ta == tam))
-    mod = NA
-  else
-    if(is.numeric(x))
-      mod = as.numeric(names(ta)[ta == tam])
-  else
-    mod = names(ta)[ta == tam]
-  return(mod)
-}
+#Change variable type and describe one by one
+das$Race5 <- as.factor(das$Race5)
+str(das$Race5)
+summary(das$Race5)
+table(das$Race5)
 
-Race5_mode <- MODE(as$Race5)
-Region_mode <- MODE(as$Region)
-Edu8_mode <- MODE(as$Edu8)
-Income_mode <- MODE(as$Income)
+das$Region <- as.factor(das$Region)
+str(das$Region)
+summary(das$Region)
+table(das$Region)
+
+das$Edu8 <- as.factor(das$Edu8)
+str(das$Edu8)
+summary(das$Edu8)
+table(das$Edu8)
+
+das$Income <- as.factor(das$Income)
+str(das$Income)
+summary(das$Income)
+table(das$Income)
 
 #Categorical Data Visualization
 das %>%
   ggplot(aes(x = Race5))+
   geom_bar(fill = "#97B3C6", color = "black")+
   theme_bw()+
-  labs(x = NULL,
-       y = NULL,
-       title = NULL)
+  labs(x = "Race",
+       y = "Count",
+       title = NULL) + 
+  scale_fill_manual(name = "RACE", 
+                    label=c("Hispanic", "White", "Black", "West", "Other"))
+
+ggplot(data = das) +
+  geom_bar(mapping = aes(x = Race5, y = Count, fill = Race5), 
+           position = "dodge", 
+           stat = "identity", 
+           color = "black") +
+  xlab("Race") +
+  ylab("Count") +
+  scale_x_discrete(label=c("Hispanice", "White", "Black","West", "Other"))
 
 das %>%
   ggplot(aes(x = Region))+
@@ -77,22 +89,20 @@ das %>%
        y = NULL,
        title = NULL)
 
-das$Race5 <- as.numeric(das$Race5)
-
-table(df) #Background x Geographical region
-(df_tbl <- as.data.frame(table(df)))
+table(das) #Background x Geographical region
+(df_tbl <- as.data.frame(table(das)))
 
 # Background split by geographical region
 ggplot(data = df_tbl) +
-  geom_bar(mapping = aes(x = REGION_US, y = Freq, fill =  RACETHN_4CAT), 
+  geom_bar(mapping = aes(x = Region, y = Freq, fill =  Race5), 
            position = "dodge", #it is next to each other 
            stat = "identity", 
            color = "black") +
-  xlab("REGION_US") +
+  xlab("Regions of US") +
   ylab("Freq") +
   scale_x_discrete(label=c("Northeast", "Midwest", "South", "Other")) +
   scale_fill_manual(name = "RACE", 
-                    label=c("Hispanic", "White", "Black", "West"),
+                    label=c("Hispanic", "White", "Black", "West", "Other"),
                     values=c("chocolate4", "palevioletred1", "papayawhip", "Chocolate")) +
   theme(axis.text.x = element_text(size = 10))
 
@@ -100,7 +110,7 @@ ggplot(data = df_tbl) +
 #scale_fill_manual you can change the colors and find them online 
 # Birth region split by gender
 ggplot(data = df_tbl) +
-  geom_bar(mapping = aes(x = GENDER_R, y = Freq, fill = BIRTHRGNUS_C), 
+  geom_bar(mapping = aes(x = Race5, y = Freq, fill = Race5), 
            position = "dodge", 
            stat = "identity", 
            color = "black") +
