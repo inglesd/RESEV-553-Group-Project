578b212c22e2c1636f9f1bc4346132328b24d0bc
Amy Mendee <amymendee@amendee.student.iastate.edu>
2022-02-08 20:00:45 -0600

    Combined file by Amy

--- 553 Group project script.R
+++ 553 Group project script.R
@@ -4,22 +4,52 @@ library(psych)
 library(dplyr)
 library(ggplot2)
 library(scales)
-library(AggregateR)
 library(plyr)
 
 #Import the dataset ####
 library(haven)
 prgusap1_puf <- read_sas("prgusap1_puf.sas7bdat", NULL)
-
-#Prepare the dataset for analysis ####
-
-df <- subset(prgusap1_puf, select = c(RACETHN_5CAT, REGION_US, EDCAT8, J_Q09USX))
+View(prgusap1_puf)
+df <- prgusap1_puf
 
 #Rename the variables
-df$Race5 <- (df$RACETHN_5CAT)
-df$Region <- (df$REGION_US)
-df$Edu8 <-(df$EDCAT8)
-df$Income <-(df$J_Q09USX)
+#(Race5=RACETHN_5CAT, Region=REGION_US, EDU8=EDCAT8, Income=J_Q09USX)
+df_small <- df%>%
+  select(RACETHN_5CAT, REGION_US, EDCAT8, J_Q09USX) %>%
+  rename(Race=RACETHN_5CAT, Region=REGION_US, EDUC=EDCAT8, Income=J_Q09USX)
+
+#Change the name of race variable 
+df_Race <- df_small %>%
+  mutate(Race = ifelse(Race==1, "Hispanic",
+                       ifelse(Race==2, "White",
+                              ifelse(Race==3, "Black",
+                                     ifelse(Race==4, "Asian/Pacific Islander",
+                                            ifelse(Race==6, "Other",
+                                                   ifelse(Race==NA, "Not Stated")))))))
+#Change the name of region variable 
+df_Region_Race <- df_Race %>%
+  mutate(Region = ifelse(Region== 1, "Northeast",
+                         ifelse(Region== 2, "Midwest",
+                                ifelse(Region== 3, "South",
+                                       ifelse(Region== 4, "West", "Other Region")))))
+
+#Change the name of education variable 
+df_Ed_Reg_Race <- df_Region_Race %>%
+  mutate(EDUC = ifelse(EDUC==1 | EDUC==2, "Primary",
+                       ifelse(EDUC==3 | EDUC==4, "Secondary",
+                              ifelse(EDUC==6, "Bachelors",
+                                     ifelse(EDUC==7, "Masters",
+                                            ifelse(EDUC==5 | EDUC==8, "Professional",
+                                                   ifelse(EDUC==NA, "Not Stated")))))))
+#Change the name of Income variable
+df_ALL <- df_Ed_Reg_Race %>%
+  mutate(Income = ifelse(Income== 1 | Income==2, "1-19,999",
+                         ifelse(Income==3 | Income== 4, "20,000-39,999",
+                                ifelse(Income== 5 | Income==6, "40,000-59,999",
+                                       ifelse(Income==7, "60,000-74,999",
+                                              ifelse(Income==8, "75,000-99,999",
+                                                     ifelse(Income==10, "100,000-149,999",
+                                                            ifelse(Income== 11 | Income==NA, "Not Stated", "Other Income")))))))) 
 
 #Dataset for the analysis ####
 das <- subset(df, select = c(Race5, Region, Edu8, Income))
